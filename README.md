b5e3ba82411a804433e5d1b73a99b4793a2e2948
viocha <1457174277@qq.com>
2025-05-19 18:10:26 +0800

    update

---
+++ README.md
@@ -0,0 +1,15 @@
+# tsr
+
+A simple TypeScript Node.js runner
+
+## Installation
+
+```bash
+npm install -g tsr
+```
+
+## Usage
+
+```bash
+tsr <file.ts>
+```
--- test/component.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Button(){
-	return (
-		<button className="bg-blue-500 text-white px-4 py-2 rounded">
-			Click Me
-		</button>
-	)
-}
